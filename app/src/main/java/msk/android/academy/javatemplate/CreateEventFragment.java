01cf2aaa1fc5b5fda0311c32348534ba281e4c33
nein <nein_kib@mail.ru>
2018-12-16 09:04:03 +0300

    Переход после сохранения

--- app/src/main/java/msk/android/academy/javatemplate/CreateEventFragment.java
+++ app/src/main/java/msk/android/academy/javatemplate/CreateEventFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -67,6 +68,9 @@ public class CreateEventFragment extends Fragment {
 
             private void saveResult(JsonObject jsonObject) {
                 Log.d(TAG,"ok");
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, MapFragment.newInstance());
+                transaction.commit();
             }
         });
         return fragmentView;
